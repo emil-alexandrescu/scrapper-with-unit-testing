08a2b6b348a13c66c09222c285147082ee3b77c3
Emil Alexandrescu <emil.alexandresku@gmail.com>
2014-06-12 12:05:04 +0300

    amazon fixed and testing uses detectstore.js

--- nodejs/test/test_domstructure.js
+++ nodejs/test/test_domstructure.js
@@ -6,65 +6,22 @@ var Config      = require('../config.json');
 var should      = require('should');
 var Request     = require('request');
 var cheerio =    require('cheerio');
+var fs = require('fs');
+var vm = require('vm');
 var test = it;
 
-function detectStore(dom) {
-    var site_name = $(dom).find('meta[property="og:site_name"]')!=undefined ? $(dom).find('meta[property="og:site_name"]').attr('content') : null;
-    if (site_name == null) {
-        if (/Mage\.Cookies\.domain/.test($(dom).text())) site_name = "Magento";
-        if (/Amazon\.com/.test($(dom).find('title').text())) site_name = "Amazon";
-        if ($(dom).find('link[rel="shortcut icon"]') && (/demandware/.test($(dom).find('link[rel^="shortcut"]').attr('href')))) site_name = "Demandware";
-        if ($(dom).find("input[name^=\"\/atg\/commerce\"]").length>0) site_name = "ATGCommerce";
-    }
-    return site_name;
+function include(path) {
+    var code = fs.readFileSync(path, 'utf-8');
+    vm.runInThisContext(code, path);
 }
 
+include ('../detectstore.js');
 
-//Checking Dom strucutre (for now, check with only price . if scrapped price is in number format, we can say dom structure is not changed.
-function testFunc(dom, site_name){
-    var price;
-    console.log("Checking " + site_name + "...");
-    switch (site_name){
-        case "Walmart.com":
-            price = $(dom).text().match(/currentItemPrice:[\s]*([\d\.]+)/)[1];
-            break;
-        case "Magento":
-            var current_price_match = $(dom).text().match(/"basePrice":"([\d\.]+)"/);
-            price = current_price_match?current_price_match[1]:"";
-            break;
-        case "Amazon":
-            var current_price_match = $(dom).find('#priceblock_ourprice').text().match(/\$([\d\.]+)/);
-            price = current_price_match?current_price_match[1]:"";
-            break;
-        case "Target":
-            price = $(dom).find('span.offerPrice').text().substr(1).match(/[\d\.]+/)[0];
-            break;
-        case "Demandware":
-            price = $(dom).find('.product-price').find('span[class^=price-]').text().match(/[\d\.]+/)[0];
-            break;
-        case "ATGCommerce":
-            $(dom).find("[class$=-price]").each(function(){
-                if (m = $(this).text().match(/([\d]+\.[\d]+)|([\d]+)/)){
-                    price = m[0];
-                    return false;
-                }
-            });
-            break;
-        case "Best Buy":
-            price = $(dom).find('div.regular-price').text().match(/[0-9\.]+/)[0];
-            break;
-    }
-    console.log(price);
-    (price == price*1).should.be.ok;
-}
-// ----------------
-//   Test Data
-// ----------------
 
 var TestData = [
         {
             "url": "www.walmart.com/ip/West-Bend-0.9-cu.-ft.-900-Watt-Microwave/24950645",
-            "site": "Walmart.com"
+            "site": "Walmart"
         },
         {
             "url": "www.petermillar.com/men/men-collections/new-arrivals/plaid-justice-sport-coat-italian-wool-blend.html",
@@ -113,9 +70,24 @@ describe('Page Check :', function() {
                 } else {
                     r.statusCode.should.equal(200);
                     $ = cheerio.load(body);
-                    var site_name = detectStore($('html')[0]);
-                    site_name.should.equal(data['site']);
-                    testFunc($('html')[0], site_name);
+                    var storeparser;
+                    var product_info;
+                    try{
+                        storeparser = detectStore($('html')[0]);
+                        product_info = storeparser.get_product_page_info($('html')[0]);
+                    }catch(e){
+                        console.log(e);
+                        product_info = {};
+                    }
+                    console.log(storeparser);
+                    storeparser.store.should.equal(data['site']);
+                    product_info['id'].should.be.ok;
+                    product_info['product_name'].should.be.ok;
+                    product_info['sku'].should.be.ok;
+                    product_info['current_price'].should.be.ok;
+                    (product_info['current_price'] == product_info['current_price']*1).should.be.ok;
+                    product_info['images'].should.be.an.Array;
+                    //testFunc($('html')[0], site_name);
                     done();
                 }
             });
