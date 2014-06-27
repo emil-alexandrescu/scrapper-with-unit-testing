0de78fb603e990f9d974e42f912deb3b72d8a657
Emil Alexandrescu <emil.alexandresku@gmail.com>
2014-06-27 17:40:37 +0300

    extension - add all from cart

--- extension/extension.js
+++ extension/extension.js
@@ -7,14 +7,22 @@ appAPI.ready(function($) {
     appAPI.resources.includeRemoteJS('http://cartbox.parseapp.com/src/common/addproduct/convertimage.js');
     appAPI.resources.includeRemoteJS('http://cartbox.parseapp.com/src/common/addproduct/detectstore.js');
     appAPI.resources.includeRemoteJS('http://cartbox.parseapp.com/src/common/addproduct/productsave.js');
+    appAPI.resources.includeRemoteJS('http://cartbox.parseapp.com/src/common/addproduct/detectstore_addfromcart.js');
     appAPI.resources.includeJS('toastr.min.js');
     appAPI.resources.includeCSS('toastr.min.css');
     appAPI.resources.includeRemoteJS('http://www.parsecdn.com/js/parse-1.2.18.min.js');
+    
+    
     //pare initialize
     Parse.initialize("YCV5ZQm2HBkJ3ugHCDwULH75Nb3NVanr3QscKXXE", "ChwfTXDXKo4UjoSYDbYNWtEFCmoEBraltnYUtVSc");
-
+    
+    //declaration of product array in cart
+    var cartbox_array =[];
+    
     //detect user if possible
     var currentuser = Parse.User.current();
+
+    
     if (currentuser){
         appAPI.db.set("usertoken", currentuser._sessionToken);
         console.log(currentuser._sessionToken);
@@ -29,67 +37,105 @@ appAPI.ready(function($) {
             });
         }
     }
-
+    
+    //get items from cartbox
     var storeParser=detectStore(document);
-    var product_info;
-    console.log(storeParser);
-    if (storeParser!=null){
-        console.log('************** visiting ' + storeParser.store + ' store ');
-        try{
-            product_info = storeParser.get_product_page_info(document);
-        }catch(e){
-            console.log(e);
-            product_info = null;
-        }
-        if (product_info!=null && product_info['product_name']){
-            toastr.options = {
-                "closeButton": true,
-                "debug": false,
-                "positionClass": "toast-bottom-right",
-                "onclick": null,
-                "showDuration": "300",
-                "hideDuration": "1000",
-                "timeOut": "50000",
-                "extendedTimeOut": "10000",
-                "showEasing": "swing",
-                "hideEasing": "linear",
-                "showMethod": "fadeIn",
-                "hideMethod": "fadeOut"
+    storeParser.get_cart_box(document, function(arr){
+        //cloning array
+        if ($.isArray(arr))
+            cartbox_array = arr.slice(0);
+        
+        var product_info;
+        if (storeParser!=null){
+            console.log('************** visiting ' + storeParser.store + ' store ');
+            try{
+                product_info = storeParser.get_product_page_info(document);
+            }catch(e){
+                console.log(e);
+                product_info = null;
             }
-            toastr.info(storeParser.store +' product found. <br /><button class="btn-info" id="add-to-cartbox">ADD TO CARTBOX</button>');
-        }
-    }
-    $('#add-to-cartbox').live('click', function(){
-
-
-        //check if the user is logged in
-        var Product = Parse.Object.extend("Product");
-        var ProductImage = Parse.Object.extend("ProductImage");
-        var User = Parse.Object.extend("User");
-        var userQuery = new Parse.Query(User);
-        //var currentuser = Parse.User.current();
-
-        if (currentuser != null && currentuser !=undefined){
-            console.log(currentuser);
-            console.log(currentuser.get("username"));
-
-            userQuery.equalTo("username", currentuser.get("username"));
-            userQuery.count({
-                success:function(count){
-                    if (count === 0){
-                        alert("Please log in to add to cartbox");
-                        document.location.href = "http://cartbox.parseapp.com/#/signin";
-                        return;
-                    }else{
-                        var url = document.location.href;
-                        productSave(product_info, storeParser, url, currentuser );
-                    }
+            if (product_info!=null && product_info['product_name']){
+                toastr.options = {
+                    "closeButton": true,
+                    "debug": false,
+                    "positionClass": "toast-bottom-right",
+                    "onclick": null,
+                    "showDuration": "300",
+                    "hideDuration": "1000",
+                    "timeOut": "50000",
+                    "extendedTimeOut": "10000",
+                    "showEasing": "swing",
+                    "hideEasing": "linear",
+                    "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
                 }
-            });
-        }else{
-            alert("Please log in to add to cartbox");
-            document.location.href = "http://cartbox.parseapp.com/#/signin";
-            return;
+                var info = storeParser.store +' product found. <br /><button class="btn-info" id="add-to-cartbox">ADD TO CARTBOX</button>';
+                if (arr!=null &&  ($.isArray(arr)) && arr.length>0) {
+                    info += '<br /><button class="btn-info" id="add-all-from-cart">ADD ALL FROM CART</button>';
+                }
+                toastr.info(info);
+            }
         }
+    })
+    
+    $('#add-all-from-cart').live('click', function(){
+        //check if the user is logged in
+        var User = Parse.Object.extend("User");
+           var userQuery = new Parse.Query(User);
+           if (currentuser != null && currentuser !=undefined){
+               userQuery.equalTo("username", currentuser.get("username"));
+               userQuery.count({
+               success:function(count){
+                   if (count === 0){
+                       alert("Please log in to add to cartbox");
+                       document.location.href = "http://cartbox.parseapp.com/#/signin";
+                       return;
+                   }else{
+                       for (var i=0; i<cartbox_array.length; i++){
+                           $.ajax({url: cartbox_array[i]}).done(function(response, textstatus, request){
+                               var parser = new DOMParser();
+                            var dom = parser.parseFromString(response, "text/html");
+                            var sp = detectStore(dom);
+                               var p_info = sp.get_product_page_info(dom);
+                               var p_url = this.url;
+                               if (p_url.substr(0,1) == "/") p_url = "http://" + location.hostname + p_url;
+                               console.log(p_url);
+                               productSave(p_info, sp, p_url, currentuser );
+                           });
+                       }                
+                   }
+               }
+               });
+           }else{
+               alert("Please log in to add to cartbox");
+               document.location.href = "http://cartbox.parseapp.com/#/signin";
+               return;
+           }
     });
+    
+    $('#add-to-cartbox').live('click', function(){
+        //check if the user is logged in
+        var User = Parse.Object.extend("User");
+           var userQuery = new Parse.Query(User);
+           if (currentuser != null && currentuser !=undefined){
+               userQuery.equalTo("username", currentuser.get("username"));
+               userQuery.count({
+               success:function(count){
+                   if (count === 0){
+                       alert("Please log in to add to cartbox");
+                       document.location.href = "http://cartbox.parseapp.com/#/signin";
+                       return;
+                   }else{
+                       var url = document.location.href;
+                    productSave(product_info, storeParser, url, currentuser );
+                   }
+               }
+               });
+           }else{
+               alert("Please log in to add to cartbox");
+               document.location.href = "http://cartbox.parseapp.com/#/signin";
+               return;
+           }
+    });
+
 });
